ee02580963931edd20ba4e58338bdf28ea8d3c05
cagoncil <crystallophobia@gmail.com>
2022-07-21 15:34:37 -0700

    commit DOM manipulation edits

--- main.js
+++ main.js
@@ -6,36 +6,57 @@ const modal = document.createElement('div');
     modal.setAttribute('class', 'fadeOut')
 const content = document.createElement('div')
     content.setAttribute('id', 'modal-content')
-    // content.setAttribute('class', 'animated')
-const x = document.createElement('span');
-    x.setAttribute('class', 'close');
-    x.innerText = '✖';
-    // <span class='close'>✖</span>
 
-    body.appendChild(modal);
-    modal.appendChild(content);
-    content.appendChild(x);
-    const header = document.createElement('h1');
-    content.appendChild(header);
-   
-function selfCaring(){
-    //declare random msg
-    const randomMsgArray = ["Touch some GRASS", "Close your EYES", "Pet your PET/S", "Give a HUG"];
+// const x = document.createElement('span');
+//     x.setAttribute('class', 'close');
+//     x.innerText = '✖';
 
-    const randomIndex = Math.round(Math.random()*(randomMsgArray.length - 1));
-    const randomMsg = randomMsgArray[randomIndex];
-    
-    //populate random msg as header inside body
-    header.innerText = randomMsg;
-    // body.innerHTML =  '';
 
-    console.log(randomMsg, randomIndex);
-    toggleModal();
-//const testText = `<h1>${randomMsg[Math.round(Math.random()*randomMsg.length-1)]}</h1>`
+body.appendChild(modal);
+modal.appendChild(content);
+// content.appendChild(x);
+const header = document.createElement('h1');
+content.appendChild(header);
 
-//testing randomMsg
-//console.log(randomMsg[Math.round(Math.random()*randomMsg.length-1)]);
-}
+
+const backupImgURL = 'https://images.unsplash.com/photo-1658279967815-388645755042?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1015&q=80';
+
+const sheet = (function() {
+    const style = document.createElement('style');
+    style.appendChild(document.createTextNode(""));
+    document.head.appendChild(style);
+    return style.sheet;
+})();
+    // sheet.insertRule("#modal-content::before { background-image: url('" + backupImgURL + "'); } ", 0)
+
+    const randomMsgArray = [
+      "Touch some grass", 
+      "Close your eyes", 
+      "Pet your pets", 
+      "Give someone you love a hug",
+      "Take a break",
+      "Meditate",
+      "Take a nap",
+      "Cook a healthy meal",
+      "Make a cup of tea",
+      "Go for a walk",
+      "Enjoy the sunshine",
+      "Connect with friends",
+      "Call a family member",
+      "Make your bed",
+      "Read a good book",
+      "Declutter your living space",
+      "Take a social media break",
+      "Practice deep breathing",
+      "Go to the gym",
+      "Light your favorite candle",
+      "Do some exercise or yoga",
+      "Start a gratitude journal",
+      "Take a break from screens",
+      "Listen to some music",
+      "Remember to hydrate"
+  ];
+
 
 function toggleModal() {
     // modal.style.display = 'none';
@@ -55,7 +76,7 @@ function toggleModal() {
     // }, 300);
   }
 
-  x.onclick = toggleModal;
+//   x.onclick = toggleModal;
 
   window.onclick = function(event) {
     if (event.target == modal) {
@@ -72,3 +93,45 @@ setInterval(() => {
     if (modal.classList.contains('fadeOut')) selfCaring();
 }, interval);
 
+
+function selfCaring(){
+  // const accessKey = 'r5PzuV6EnfbtvOcylKIBBrS4v4V173YUb7NXFrzktns';
+  const url = 'https://api.unsplash.com/photos/random?client_id=r5PzuV6EnfbtvOcylKIBBrS4v4V173YUb7NXFrzktns';
+  fetch(url)
+    .then(data => data.json())
+    .then(data => {
+      console.log('fetching data...');
+      console.log(data);
+      const imageURL = data.urls.full;
+
+      console.log(imageURL);
+      sheet.insertRule("#modal-content::before { background-image: url('" + imageURL + "'); } ", 0)
+  
+      const randomIndex = Math.round(Math.random()*(randomMsgArray.length - 1));
+      const randomMsg = randomMsgArray[randomIndex];
+      
+      //populate random msg as header inside body
+      header.innerText = randomMsg;
+      // body.innerHTML =  '';
+  
+      console.log(randomMsg, randomIndex);
+      toggleModal();
+
+    })
+    .catch(err => {
+      console.log('fetch failed, calling backup img');
+      sheet.insertRule("#modal-content::before { background-image: url('" + backupImgURL + "'); } ", 0)
+  
+      const randomIndex = Math.round(Math.random()*(randomMsgArray.length - 1));
+      const randomMsg = randomMsgArray[randomIndex];
+      
+      //populate random msg as header inside body
+      header.innerText = randomMsg;
+      // body.innerHTML =  '';
+  
+      console.log(randomMsg, randomIndex);
+      toggleModal();
+    })
+}
+  
+  
